e89c419cea0d24143b2b06900d165f0afc977bf6
Ryan Pethel <arpethel@gmail.com>
2019-03-24 20:14:21 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -26,12 +26,11 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
   regex_beginning = /\A[A-Z]+\w/
   regex_end = /\w+[!.?]+\Z/
   text_array = text.split
-  # binding.pry
-  if text_array[0].match?(regex_beginning) && text_array[-1].match?(regex_end)
-    true
-  else
-    false
-  end
+  return text_array[0].match?(regex_beginning) && text_array[-1].match?(regex_end)
+  #   true
+  # else
+  #   false
+  # end
 
 end
 
