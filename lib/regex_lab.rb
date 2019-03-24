a2b531641d7d890d61c8f5b0ea0717dc3511ac2a
Ryan Pethel <arpethel@gmail.com>
2019-03-24 19:07:32 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,13 +14,9 @@ def words_starting_with_un_and_ending_with_ing(text_string)
   regex_downcase = /\A(un)+\w+(ing)/i
   regex_upcase = /\A(Un)+\w+(ing)/i
   text_string.split.each do |word|
-    array << word if word.match?(regex_downcase) || word.match?(regex_upcase)
-    #   array << word
-    # else
-    #   array
-    # end
+    return array << word if word.match?(regex_downcase) || word.match?(regex_upcase)
   end
-  array
+  # array
 end
 
 def words_five_letters_long(text)
